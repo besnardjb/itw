2833bd190eb7ae738c6d0c2154b4f0fc5d380dc1
Jean-Baptiste BESNARD <jbesnard@ddn.com>
2025-08-07 09:26:36 +0200

    EX5

---
+++ ex5/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+
+class Shape
+{
+    public:
+        virtual double surface() const = 0;
+        virtual void print() const = 0;
+};
+
+/* TODO implemen square and triangle */
+
+
+int main(void)
+{
+    /* TODO create a vector of shapes  (triangle and square)*/
+    /* TODO print the surface of all shapes and total */
+
+    return 0;
+}
