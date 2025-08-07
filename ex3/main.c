9c653e63fecd16909af1fb2c2011fe23639bae66
Jean-Baptiste BESNARD <jbesnard@ddn.com>
2025-08-07 09:11:00 +0200

    EX3

---
+++ ex3/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    /* Print all arguments in reverse order
+    *  also reverse the order of characters in each argument */
+
+    /* Then rerun the *same* program with the reversed arguments */
+
+    return 0;
+}
